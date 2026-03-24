89adabd13745155a46c7b02a0edf4b8b6f6b0d21
NgoQuoc04 <ngochiquoc140@gmail.com>
2026-03-24 13:58:22 +0700

    typescript

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("quoc")
+const express = require("express")
+
+const app = express()
+const PORT = 8080
+
+app.get("/" , (req, res) => {
+    res.send("HEllO")
+})
+
+app.listen(8080, () => {
+    console.log(`My app is running on port: ${PORT}`)
+})
